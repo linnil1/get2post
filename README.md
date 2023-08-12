3e08250926b0c6547e81662abe250ff79e9f94d4
linnil1 <linnil1.886@gmail.com>
2023-08-12 20:21:15 +0800

    Add unflatten feature for query

--- README.md
+++ README.md
@@ -1,21 +1,36 @@
 # GET to POST service in GoLang
 
-Flow: GET -> aws APIGateway -> aws Lambda -> [Lambda web adapter](https://github.com/awslabs/aws-lambda-web-adapter/tree/main) -> [GoLang Binary](./app) -> check secret -> POST -> target web
+Flow: GET (query) -> aws APIGateway -> aws Lambda -> [Lambda web adapter](https://github.com/awslabs/aws-lambda-web-adapter/tree/main) -> [GoLang Binary](./app) -> check secret -> POST (json) -> target web
 
 
 ## Deploy
 
 ``` bash
 sam build
-sam deploy --guide --parameter-overrides APP_SECRET={{password}}
+sam deploy --guide --parameter-overrides SECRET={{password}}
 ```
 
-Then try it out:
-(This example is to trigger Discord webhook by GET)
+Then try it out with [httpbin](https://github.com/postmanlabs/httpbin):
 
+``` bash
+curl "https://{{your_apigateway_id}}.execute-api.{{your_region}}.amazonaws.com/get2post?secret={{password}}&url=https://httpbin.org/post&data.content=Test%20from%20aws&data.user.name=Testing&data.user.id=123"
 ```
-curl "https://{{your_apigateway_id}}.execute-api.us-east-2.amazonaws.com/get2post?secret={{password}}&url=https://discord.com/api/webhooks/{{your_webhook_id_and_token}}&content=Test%20from%20aws"
+
+The resulting POST body will be structured as follows:
+``` json
+{
+  "content": "Test from aws",
+  "user": {
+    "id": "123",
+    "name": "Testing"
+}
 ```
+
+Parameter Explanations:
+* `secret`: The required password for activating the service. Leave it empty if `SECRET` is not provided.
+* `url`: The destination URL for posting JSON data.
+* `data.*`: Parameters for supplying flattened POST data. Refer to https://github.com/nqd/flat for syntax details.
+
 
 
 ## Environment
--- app/main.go
+++ app/main.go
@@ -5,12 +5,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/nqd/flat"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
-
-	"github.com/gin-gonic/gin"
 )
 
 func retrieveAndDel(key string, queryParams *url.Values) (string, error) {
@@ -23,17 +23,26 @@ func retrieveAndDel(key string, queryParams *url.Values) (string, error) {
 	return value, nil
 }
 
-func ParamsToJson(queryParams *url.Values) ([]byte, error) {
-	// Transform parameters into json
-	queryParamsShrink := map[string]string{}
+func ExtractDataFromParams(queryParams *url.Values) (map[string]interface{}, error) {
+	// Transform parameters into map
+	// Step1: url.Values -> map[string]string{}
+	queryParamsShrink := make(map[string]interface{})
 	for k, v := range *queryParams {
 		queryParamsShrink[k] = v[0]
 	}
-	jsonData, err := json.Marshal(queryParamsShrink)
+
+	// step2: unflatten
+	queryParamsDict, err := flat.Unflatten(queryParamsShrink, nil)
 	if err != nil {
-		return []byte{}, errors.New("Failed to convert to JSON")
+		return nil, errors.New("Failed to unflatten")
 	}
-	return jsonData, nil
+
+	// step3: Extract 'data' key
+	data, ok := queryParamsDict["data"]
+	if !ok {
+		return nil, errors.New("data not provided")
+	}
+	return data.(map[string]interface{}), nil
 }
 
 func PostJson(targetURL string, jsonData []byte) (string, error) {
@@ -75,9 +84,17 @@ func main() {
 			return
 		}
 
-		// To json
-		jsonData, err := ParamsToJson(&queryParams)
+		// Extract Data
+		queryParamsDict, err := ExtractDataFromParams(&queryParams)
 		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// map -> json string
+		jsonData, err := json.Marshal(queryParamsDict)
+		if err != nil {
+			err = errors.New("Failed to convert to JSON")
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
